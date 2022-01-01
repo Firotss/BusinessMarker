6c9f4625ddc86d174cf169cef019a1d302bc6296
Firotss <jannadom8@gmail.com>
2022-01-01 17:17:45 +0200

    Create user database

--- BusinessMarker/startpage/models.py
+++ BusinessMarker/startpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    pasword = models.CharField(max_length=200)
+    reg_date = models.DateTimeField()
+    account_type = models.CharField(max_length=200)
