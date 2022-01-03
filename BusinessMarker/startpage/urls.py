c1b403526f2069ab48c9336329bd79084801706d
Firotss <jannadom8@gmail.com>
2022-01-03 08:20:31 +0200

    created log/reg backend

--- BusinessMarker/startpage/urls.py
+++ BusinessMarker/startpage/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/check', views.login_check, name='login_check'),
+    path('login/reg', views.registration, name='registration'),
+    path('login/logout', views.logout_view, name='logout'),
+    path('login/', views.loginn, name='login'),
     path('phpmyadmin/', views.phpmyadmin),
 ]
--- BusinessMarker/startpage/views.py
+++ BusinessMarker/startpage/views.py
@@ -3,12 +3,51 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 def index(request):
     return render(request, 'index.html')
 
 def phpmyadmin(request):
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
 
-def login(request):
-    return render(request, 'login.html')
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'user_profile.html')
+    else:
+        return render(request, 'login.html')
+    
+
+def loginn(request):
+    if request.user.is_authenticated:
+        return profile(request)
+    else:     
+        return render(request, 'login.html')
+
+    
+def login_check(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'user_profile.html')
+    else:
+        return render(request, 'user_profile.html')
+
+        
+        
+def registration(request):
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+            email = request.POST['email']
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            return render(request, 'user_profile.html')
+        except:
+            return HttpResponse('fail')
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'index.html')
