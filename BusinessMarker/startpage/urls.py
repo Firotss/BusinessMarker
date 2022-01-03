296a1c59e8fdbc40ba1e6cd634f55d900877cf16
Firotss <jannadom8@gmail.com>
2022-01-03 15:38:51 +0200

    code optimization

--- BusinessMarker/startpage/urls.py
+++ BusinessMarker/startpage/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/logout', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('login/check', views.login_check, name='login_check'),
-    path('login/reg', views.registration, name='registration'),
     path('login/', views.login_view, name='login'),
     path('phpmyadmin/', views.phpmyadmin),
 ]
--- BusinessMarker/startpage/views.py
+++ BusinessMarker/startpage/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
+from django.views.generic import TemplateView
 def index(request):
     return render(request, 'index.html')
 
@@ -16,37 +16,27 @@ def profile(request):
     if request.user.is_authenticated:
         return render(request, 'user_profile.html')
     else:
-        return HttpResponse(request.user.is_authenticated)
-    
-def login_view(request):
-    if request.user.is_authenticated is False:  
-        return render(request, 'login.html')
-    else:
-        return redirect("/profile/")
+        return redirect("/login")
 
-def login_check(request):
-    if request.user.is_authenticated is False:  
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            login(request, user)
-        return redirect("/login/")
-    else:
-        return redirect("/profile/")
-
-def registration(request):
-    if request.user.is_authenticated is False:  
-        try:
-            user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
-            user.save()
-            login(request, user)
-            return redirect("/profile/")
-        except:
-            return redirect('/login/')
-    else:
-        return redirect("/profile/")
-
-        
 def logout_view(request):
     logout(request)
     return redirect('/')
 
+def login_view(request):
+        if request.user.is_authenticated is False: 
+                user = None
+                try:
+                    user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+                    user.save()
+                except:
+                    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+                finally:
+                    if user is not None:
+                        login(request, user)
+                        return redirect('/profile/')
+                    return render(request, 'login.html')
+        else:
+            return redirect('/profile/')
+                
+
+       
