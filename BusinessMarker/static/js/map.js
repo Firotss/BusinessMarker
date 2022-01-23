2413b23db36c00e690adfe9fd2de46f136e34de8
Firotss <jannadom8@gmail.com>
2022-01-23 18:38:50 +0200

    Added map(test version)

---
+++ BusinessMarker/static/js/map.js
@@ -0,0 +1,33 @@
+
+require([
+"esri/widgets/Sketch",
+"esri/Map",
+"esri/layers/GraphicsLayer",
+"esri/views/MapView"
+], (Sketch, Map, GraphicsLayer, MapView) => {
+const graphicsLayer = new GraphicsLayer();
+
+const map = new Map({
+    basemap: "topo-vector",
+    layers: [graphicsLayer]
+});
+
+const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 5,
+    center: [90, 45]
+});
+
+view.when(() => {
+    const sketch = new Sketch({
+    layer: graphicsLayer,
+    view: view,
+    // graphic will be selected as soon as it is created
+    creationMode: "update"
+    });
+
+    view.ui.add(sketch, "top-right");
+});
+});
+ 
